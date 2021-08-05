a1e04a566c33734776d5b8f81c2f867a76c18131
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-08-05 21:55:12 +0530

    Finished Header and responsive mobile navigation

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,23 @@
-console.log("Hello")
+const btn = document.getElementById("btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeEls = document.querySelectorAll(".has-fade");
+const body = document.querySelector("body");
+
+btn.addEventListener("click", () => {
+  if (header.classList.contains("open")) {
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeEls.forEach((el) => {
+      el.classList.remove("fade-in");
+      el.classList.add("fade-out");
+    });
+  } else {
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeEls.forEach((el) => {
+      el.classList.add("fade-in");
+      el.classList.remove("fade-out");
+    });
+  }
+});
